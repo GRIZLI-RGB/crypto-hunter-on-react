b09de557eb8793ab3344399a481a05456e1da3c9
Evgeniy Kozlov <ek916569@gmail.com>
2023-01-10 16:35:02 +0500

    commit

---
+++ src/config/data.js
@@ -0,0 +1,18 @@
+export const chartDays = [
+    {
+        label: "24 часа",
+        value: 1,
+    },
+    {
+        label: "30 дней",
+        value: 30,
+    },
+    {
+        label: "3 месяца",
+        value: 90,
+    },
+    {
+        label: "1 год",
+        value: 365,
+    },
+];
